b966509fcc8d96753285424278e613030396aa11
Christian Kienle <christian.kienle@sap.com>
2018-12-03 15:33:00 +0100

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Fundamental-Vue</h1>
+    <FdButton @click="showModal">Show Modal</FdButton>
+    <FdModal title="Modal Title" :active.sync="isModalActive">
+      <p>Do you want to invite your friends to join the party?</p>
+      <template slot="actions">
+        <FdButton @click="closeModal" styling="light">Cancel</FdButton>
+        <FdButton @click="closeModal" styling="emphasized">Invite Friends</FdButton>
+      </template>
+    </FdModal>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isModalActive: false,
+    };
+  },
+  methods: {
+    closeModal() {
+      this.isModalActive = false;
+    },
+    showModal() {
+      this.isModalActive = true;
+    },
+  },
+};
+</script>
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App.vue'
+import FundamentalVue from 'fundamental-vue';
+
+Vue.use(FundamentalVue);
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
